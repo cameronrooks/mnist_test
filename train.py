a09b0f6608547f2b566e49a1c33a6245a4152735
unknown <cameronarooks@gmail.com>
2022-05-17 17:23:27 -0400

    added gpu support for training

--- train.py
+++ train.py
@@ -1,10 +1,14 @@
 import model
+import sys
 import numpy as np
 import torch
 import torchvision
 import torchvision.datasets as datasets
 from torchvision import transforms
 
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 transform = transforms.Compose([
     transforms.ToTensor()
@@ -13,14 +17,16 @@ transform = transforms.Compose([
 trainset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
 testset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
 
-training_loader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=0)
+
+training_loader = torch.utils.data.DataLoader(trainset, batch_size=10, shuffle=True, num_workers = 0)
 
 model = model.Net()
+model.to(device)
 optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 loss_fn = torch.nn.MSELoss()
 
-
-for x in range(100):
+#training loop
+for x in range(30):
 
 	running_loss = 0
 
@@ -28,6 +34,9 @@ for x in range(100):
 	for i, data in enumerate(training_loader):
 		#get the inputs and their labels
 		inputs, labels = data
+		#move to gpu
+		inputs, labels = inputs.to(device), labels.to(device)
+
 
 		#zero the gradients
 		optimizer.zero_grad()
@@ -43,6 +52,7 @@ for x in range(100):
 
 		optimizer.step()
 
-	if x%10 == 0:
-		print(running_loss)
+	print("epoch number: " + str(x))
+	print(running_loss)
+	sys.stdout.flush()
 
