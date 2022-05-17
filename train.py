e202192b59dca4c2ed277de1d3eb1cb3a9d0b75c
cameronrooks <43239687+cameronrooks@users.noreply.github.com>
2022-05-17 14:53:29 -0400

    created training loop

--- train.py
+++ train.py
@@ -1,5 +1,4 @@
 import model
-from mnist import MNIST
 import numpy as np
 import torch
 import torchvision
@@ -16,8 +15,34 @@ testset = datasets.MNIST(root='./data', train=False, download=True, transform=tr
 
 training_loader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=0)
 
-my_nn = model.Net()
+model = model.Net()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+loss_fn = torch.nn.MSELoss()
 
 
-#for i, data in enumerate(training_loader):
+for x in range(100):
+
+	running_loss = 0
+
+
+	for i, data in enumerate(training_loader):
+		#get the inputs and their labels
+		inputs, labels = data
+
+		#zero the gradients
+		optimizer.zero_grad()
+
+		#make predictions
+		pred = model(inputs)
+
+		#calculate loss
+		loss = torch.nn.functional.nll_loss(pred, labels)
+		loss.backward()
+
+		running_loss += loss.item()
+
+		optimizer.step()
+
+	if x%10 == 0:
+		print(running_loss)
 
